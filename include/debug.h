3961ac02a4e7de26de26a5a2d86125fb5bd41cc1
mbjd <balduindettling@gmail.com>
2016-08-06 12:44:29 +0200

    moved escaped \n's left

--- include/debug.h
+++ include/debug.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 #define DEBUG 1
-#define PRINT_DEBUG(str)                                                       \
-	if (DEBUG)                                                                 \
-	{                                                                          \
-		printf("DEBUG: %s\n", str);                                            \
+#define PRINT_DEBUG(str)            \
+	if (DEBUG)                      \
+	{                               \
+		printf("DEBUG: %s\n", str); \
 	}
--- test/minunit.h
+++ test/minunit.h
@@ -3,20 +3,20 @@
  * http://www.jera.com/techinfo/jtns/jtn002.html
  */
 
-#define mu_assert(message, test)                                               \
-	do                                                                         \
-	{                                                                          \
-		if (!(test))                                                           \
-			return "Test failed: " message;                                    \
+#define mu_assert(message, test)            \
+	do                                      \
+	{                                       \
+		if (!(test))                        \
+			return "Test failed: " message; \
 	} while (0)
 
-#define mu_run_test(test)                                                      \
-	do                                                                         \
-	{                                                                          \
-		char* message = test();                                                \
-		tests_run++;                                                           \
-		if (message)                                                           \
-			return message;                                                    \
+#define mu_run_test(test)       \
+	do                          \
+	{                           \
+		char* message = test(); \
+		tests_run++;            \
+		if (message)            \
+			return message;     \
 	} while (0)
 
 extern int tests_run;
