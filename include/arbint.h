d6ac8065df06e5de52c39cdaf87e4fc91930db15
mbjd <balduindettling@gmail.com>
2016-08-11 16:52:42 +0200

    header for said function

--- include/arbint.h
+++ include/arbint.h
@@ -8,6 +8,11 @@
 
 void str_to_arbint(char* input_str, arbint* to_fill, uint32_t base);
 
+// Deallocates an arbint struct and its value.
+void arbint_free(arbint* to_free);
+
+// Deallocates the value only, for when the struct was statically allocated.
+void arbint_free_static(arbint* to_free);
 void arbint_init(arbint* new_arbint);
 
 void arbint_mul(arbint* to_mul, uint32_t multiplier);
