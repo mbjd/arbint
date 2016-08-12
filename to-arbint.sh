14b2ed81848d1b6b1a6b9b360df8d17f73fc3b67
mbjd <balduindettling@gmail.com>
2016-08-13 00:41:02 +0200

    little explanation

--- to-arbint.sh
+++ to-arbint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # quick dirty script to see the arbint representation of a number
 # you can input a number or a bc expression
+# If you're on linux, you have to change 'gtac' to tac
+# (On mac the coreutils package from homebrew prefixes all commands with 'g')
 
 for i in $(echo "obase=2; $1" | bc | tr -d '\\\n' | rev | fold -w32 | rev); do
 	echo "ibase=2;$i" | bc
