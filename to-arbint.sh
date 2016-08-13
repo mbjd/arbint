55f3f4685ad7be26922dd150dabb0b2059bd2985
mbjd <balduindettling@gmail.com>
2016-08-13 20:20:28 +0200

    more explanatory comments

--- to-arbint.sh
+++ to-arbint.sh
@@ -4,7 +4,10 @@
 # If you're on linux, you have to change 'gtac' to tac
 # (On mac the coreutils package from homebrew prefixes all commands with 'g')
 
+# Convert to binary and split to 32-bit parts
 for i in $(echo "obase=2; $1" | bc | tr -d '\\\n' | rev | fold -w32 | rev); do
+	# Convert each of those parts back to decimal
 	echo "ibase=2;$i" | bc
+# Number lines and reverse
 done | nl -v0 | gtac | column -t | sed 's/^/(2^32)^/g;s/  / * /g'
 
